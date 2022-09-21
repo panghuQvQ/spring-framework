a5e113a1eddb09b74388064afb1b41f08260e11f
panghuQvQ <1097155803@QQ.com>
2022-09-21 21:01:39 +0800

    JVM

---
+++ tuling/src/main/java/com/zhouyu/jvm/Math.java
@@ -0,0 +1,28 @@
+package com.zhouyu.jvm;
+
+import com.zhouyu.service.User;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName Math.java
+ * @Description TODO
+ * @createTime 2022年09月21日 13:39:00
+ */
+public class Math {
+
+	public static int initData = 666;
+	public static User user = new User();
+
+	public int compute() { // 一个方法对应一块栈帧内存区域
+		int a = 1;
+		int b = 2;
+		int c = (a + b) * 10;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Math math = new Math();
+		math.compute();
+	}
+}
---
+++ tuling/src/main/java/com/zhouyu/jvm/User1.java
@@ -0,0 +1,44 @@
+package com.zhouyu.jvm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName User.java
+ * @Description TODO
+ * @createTime 2022年07月14日 15:48:00
+ */
+@Component
+public class User1 {
+
+	@Autowired
+    User1 user;
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Transactional
+	public String test() {
+		jdbcTemplate.execute("insert into student values(4,'小王','男')");
+
+		try {
+			user.test1();
+		} catch (Exception e) {
+			TransactionAspectSupport.currentTransactionStatus().setRollbackOnly(); // 强制回滚
+			return "新增失败";
+		}
+		return "新增成功";
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void test1() {
+		jdbcTemplate.execute("insert into student values(5,'小花','女')");
+		int result = 100 / 0;
+	}
+}
