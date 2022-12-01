5cac59e8dfca4fdad83165c25d15e7b6826ba41a
panghuQvQ <1097155803@QQ.com>
2022-12-01 16:04:52 +0800

    Spring AOP

--- tuling/src/main/java/com/zhouyu/jvm/MyClassLoaderTest.java
+++ tuling/src/main/java/com/zhouyu/jvm/MyClassLoaderTest.java
@@ -13,8 +13,8 @@ public class MyClassLoaderTest {
 
 		private byte[] loadByte(String name) throws Exception {
 			name = name.replaceAll("\\.", "/");
-			FileInputStream fis = new FileInputStream(classPath + "/" + name
-					+ ".class");
+
+			FileInputStream fis = new FileInputStream(classPath + "/" + name + ".class");
 			int len = fis.available();
 			byte[] data = new byte[len];
 			fis.read(data);
@@ -22,27 +22,27 @@ public class MyClassLoaderTest {
 			return data;
 		}
 
+		// 寻找类进行加载
 		protected Class<?> findClass(String name) throws ClassNotFoundException {
 			try {
 				byte[] data = loadByte(name);
-				//defineClass将一个字节数组转为Class对象，这个字节数组是class文件读取后最终的字节数组。
+				// defineClass将一个字节数组转为Class对象，这个字节数组是class文件读取后最终的字节数组
 				return defineClass(name, data, 0, data.length);
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new ClassNotFoundException();
 			}
 		}
-
 	}
 
-	public static void main(String args[]) throws Exception {
-		//初始化自定义类加载器，会先初始化父类ClassLoader，其中会把自定义类加载器的父加载器设置为应用程序类加载器AppClassLoader
-		MyClassLoader classLoader = new MyClassLoader("D:/test");
-		//D盘创建 test/com/tuling/jvm 几级目录，将User类的复制类User1.class丢入该目录
-		Class clazz = classLoader.loadClass("com.tuling.jvm.User1");
+	public static void main(String[] args) throws Exception {
+		// 初始化自定义类加载器，会先初始化父类ClassLoader,其中会把自定义类加载器的父类加载器设置为应用程序类加载器AppClassLoader
+		MyClassLoader classLoader = new MyClassLoader("D:/testFiles");
+		// D盘创建 test/com/zhouyu/jvm 几级目录，将User1类的复制类User1.class丢入该目录
+		Class<?> clazz = classLoader.loadClass("com.zhouyu.jvm.User1");
 		Object obj = clazz.newInstance();
 		Method method = clazz.getDeclaredMethod("sout", null);
-		method.invoke(obj, null);
+		method.invoke(obj,null);
 		System.out.println(clazz.getClassLoader().getClass().getName());
 	}
 }
