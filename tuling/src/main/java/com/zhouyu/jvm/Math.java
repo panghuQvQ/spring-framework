71b961b5a6d881519061449ee2bf0bed8517ef12
panghuQvQ <1097155803@QQ.com>
2022-12-01 16:04:13 +0800

    Spring AOP

--- tuling/src/main/java/com/zhouyu/jvm/Math.java
+++ tuling/src/main/java/com/zhouyu/jvm/Math.java
@@ -2,6 +2,8 @@ package com.zhouyu.jvm;
 
 import com.zhouyu.service.User;
 
+import java.util.ArrayList;
+
 /**
  * @author admin
  * @version 1.0.0
@@ -22,9 +24,26 @@ public class Math {
 	}
 
 	public static void main(String[] args) {
-		Math math = new Math();
-		while (true) {
-			math.compute();
-		}
+//		Math math = new Math();
+//		while (true) {
+//			math.compute();
+//		}
+
+//		ArrayList<String> list = new ArrayList<String>();
+//		for (int i = 0; i < 10000000; i++) {
+//			String str = String.valueOf(i).intern();
+//			list.add(str);
+//		}
+
+		String s1 = new String("he") + new String("llo");
+		String s2 = s1.intern();
+		System.out.println(s1 == s2);
+
+		String s3 = "wo";
+		String s4 = new String("wo");
+		System.out.println(s3 == s4);
+
+		String s5 = s4.intern();
+		System.out.println(s3 == s5);
 	}
 }
